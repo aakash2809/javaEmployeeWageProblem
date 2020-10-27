375fc7aa465e6844bc6dd460e77bf0a47556641c
aakash2809 <aakashrajak289@gmail.com>
2020-10-27 16:42:38 +0530

    UC_3_EmployeeDailyPartTimeWageComputation

---
+++ EmpWageBuilderUC3.java
@@ -0,0 +1,27 @@
+
+//Add part time wage calculation 
+
+public class EmpWageBuilderUC3{
+      public static void main(String args[]){
+
+         int IS_FULL_TIME = 1;                            //constants
+         int WAGE_PER_HOUR=20;                           //wage per hour
+         int FULL_DAY_HOUR=8;                            //Number of fixed Hours perday
+			int PART_TIME_HOUR=4;
+         double calculateDailyWage=0;
+
+         System.out.println("Welcome to EmployeeWage ComputationProgram");
+         double empCheck=Math.floor(Math.random() * 10)%2;//computation
+         if(empCheck == IS_FULL_TIME )                   //To check Employee is present or not
+            System.out.println("Employee is Present");
+         else
+             System.out.println("Employee is not Present");
+
+         calculateDailyWage=(FULL_DAY_HOUR*WAGE_PER_HOUR); //daily full day wage camputation
+         System.out.println("Employee daily Full day wage - " +calculateDailyWage);
+			calculateDailyWage=0;                             // assignig zero for computation  wage for part time
+         calculateDailyWage=(PART_TIME_HOUR*WAGE_PER_HOUR); //daily full day wage camputation
+         System.out.println("Employee daily Part time day wage - " +calculateDailyWage);
+
+      }
+}
